263c045eabcde7e66e1bf5ce3e5117741e9c86e5
Mohammad Talim <talimmohammad116@gmail.com>
2023-11-20 19:02:34 +0530

    Refactor: extract Title as a component

--- app/(main)/Navbar.tsx
+++ app/(main)/Navbar.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
-import { Doc, Id } from "@/convex/_generated/dataModel";
-import { useMutation, useQuery } from "convex/react";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
-import { useRef, useState } from "react";
+import Title from "./Title";
 
 interface Props {
   isCollapsed: boolean;
@@ -40,67 +38,6 @@ const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
         <Title initialData={document} />
       </div>
     </nav>
-  );
-};
-
-const Title = ({ initialData }: { initialData: Doc<"documents"> }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [isEditing, setIsEditing] = useState(false);
-  const [title, setTitle] = useState(initialData.title || "Untitled");
-  const update = useMutation(api.documents.update);
-
-  const enableEditing = () => {
-    setTitle(initialData.title);
-    setIsEditing(true);
-
-    setTimeout(() => {
-      inputRef.current?.focus();
-      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
-    }, 0);
-  };
-
-  const disableEditing = () => {
-    setIsEditing(false);
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value);
-    update({
-      id: initialData._id,
-      title: event.target.value || "Untitled",
-    });
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      disableEditing();
-    }
-  };
-
-  return (
-    <div className="flex items-center gap-x-1">
-      {!!initialData.icon && <p>{initialData.icon}</p>}
-      {isEditing ? (
-        <Input
-          ref={inputRef}
-          onBlur={disableEditing}
-          onChange={handleChange}
-          onClick={enableEditing}
-          onKeyDown={handleKeyDown}
-          value={title}
-          className="h-7 px-2 focus-visible:ring-transparent"
-        />
-      ) : (
-        <Button
-          onClick={enableEditing}
-          variant="ghost"
-          size="sm"
-          className="h-auto p-1 font-normal"
-        >
-          <span className="truncate">{initialData.title}</span>
-        </Button>
-      )}
-    </div>
   );
 };
 

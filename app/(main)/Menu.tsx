38ac643ec546e0eed6e626b0402522553b67f04c
Mohammad Talim <talimmohammad116@gmail.com>
2023-11-21 20:30:40 +0530

    Refactor: extract menu as a component

---
+++ app/(main)/Menu.tsx
@@ -0,0 +1,73 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { MoreHorizontal, Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+interface Props {
+  documentId: Id<"documents">;
+}
+
+const Menu = ({ documentId }: Props) => {
+  const { user } = useUser();
+  const router = useRouter();
+  const archive = useMutation(api.documents.archive);
+
+  const handleArchive = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.stopPropagation();
+
+    if (!documentId) return;
+
+    const promise = archive({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Error while archiving note.",
+    });
+
+    router.push("/documents");
+  };
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="sm" variant="ghost">
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent
+          className="w-60"
+          align="end"
+          alignOffset={8}
+          forceMount
+        >
+          <DropdownMenuItem onClick={handleArchive}>
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <div className="p-2 text-xs text-muted-foreground">
+            Last edited by: {user?.fullName}
+          </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Menu;
--- app/(main)/Navbar.tsx
+++ app/(main)/Navbar.tsx
@@ -1,19 +1,10 @@
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useUser } from "@clerk/clerk-react";
-import { useMutation, useQuery } from "convex/react";
-import { MenuIcon, MoreHorizontal, Trash } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
-import { toast } from "sonner";
+import { useQuery } from "convex/react";
+import { MenuIcon } from "lucide-react";
+import { useParams } from "next/navigation";
 import Banner from "./Banner";
+import Menu from "./Menu";
 import Title from "./Title";
 
 interface Props {
@@ -22,14 +13,11 @@ interface Props {
 }
 
 const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
-  const { user } = useUser();
   const params = useParams();
-  const router = useRouter();
+
   const document = useQuery(api.documents.getDocumentById, {
     documentId: params.documentId as Id<"documents">,
   });
-
-  const archive = useMutation(api.documents.archive);
 
   if (document === undefined) {
     return <p>Loading...</p>;
@@ -38,24 +26,6 @@ const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
   if (document === null) {
     return null;
   }
-
-  const handleArchive = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-  ) => {
-    event.stopPropagation();
-
-    if (!document._id) return;
-
-    const promise = archive({ id: document._id });
-
-    toast.promise(promise, {
-      loading: "Moving to trash...",
-      success: "Note moved to trash!",
-      error: "Error while archiving note.",
-    });
-
-    router.push("/documents");
-  };
 
   return (
     <>
@@ -72,31 +42,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
         <div className="items center flex w-full justify-between">
           <Title initialData={document} />
 
-          <div className="flex items-center gap-x-2">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button size="sm" variant="ghost">
-                  <MoreHorizontal className="h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-
-              <DropdownMenuContent
-                className="w-60"
-                align="end"
-                alignOffset={8}
-                forceMount
-              >
-                <DropdownMenuItem onClick={handleArchive}>
-                  <Trash className="mr-2 h-4 w-4" />
-                  Delete
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <div className="p-2 text-xs text-muted-foreground">
-                  Last edited by: {user?.fullName}
-                </div>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
+          <Menu documentId={document._id} />
         </div>
       </nav>
     </>

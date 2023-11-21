09072f7576fcba3e20d2cab019ed8f625ceee797
Mohammad Talim <talimmohammad116@gmail.com>
2023-11-21 20:07:19 +0530

    Fix: show a banner for archived documents

--- app/(main)/Navbar.tsx
+++ app/(main)/Navbar.tsx
@@ -1,8 +1,11 @@
+import ConfirmModal from "@/components/ConfirmModal";
+import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+import { toast } from "sonner";
 import Title from "./Title";
 
 interface Props {
@@ -25,19 +28,75 @@ const Navbar = ({ isCollapsed, onResetWidth }: Props) => {
   }
 
   return (
-    <nav className="flex w-full items-center gap-x-4 bg-background px-3 py-2 dark:bg-[#1f1f1f]">
-      {isCollapsed && (
-        <MenuIcon
-          role="button"
-          onClick={onResetWidth}
-          className="h-6 w-6 text-muted-foreground"
-        />
-      )}
+    <>
+      {document.isArchived && <Banner documentId={document._id} />}
+      <nav className="flex w-full items-center gap-x-4 bg-background px-3 py-2 dark:bg-[#1f1f1f]">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
 
-      <div className="items center flex w-full justify-between">
-        <Title initialData={document} />
-      </div>
-    </nav>
+        <div className="items center flex w-full justify-between">
+          <Title initialData={document} />
+        </div>
+      </nav>
+    </>
+  );
+};
+
+const Banner = ({ documentId }: { documentId: Id<"documents"> }) => {
+  const router = useRouter();
+
+  const remove = useMutation(api.documents.remove);
+  const restore = useMutation(api.documents.restore);
+
+  const handleRemove = () => {
+    const promise = remove({ documentId: documentId });
+
+    toast.promise(promise, {
+      loading: "Deleting note...",
+      success: "Note deleted!",
+      error: "Failed to delete note.",
+    });
+
+    router.push("/documents");
+  };
+
+  const handleRestore = () => {
+    const promise = restore({ documentId: documentId });
+
+    toast.promise(promise, {
+      loading: "Restoring note...",
+      success: "Note restored!",
+      error: "Failed to restore note.",
+    });
+  };
+
+  return (
+    <div className="flex w-full items-center justify-center gap-x-2 bg-rose-500 p-2 text-center text-sm text-white">
+      <p>This page is in the Trash.</p>
+
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={handleRestore}
+        className="h-auto border-white bg-transparent p-1 px-2 font-normal text-white hover:bg-primary/5 hover:text-white"
+      >
+        Restore page
+      </Button>
+      <ConfirmModal onConfirm={handleRemove}>
+        <Button
+          variant="outline"
+          size="sm"
+          className="h-auto border-white bg-transparent p-1 px-2 font-normal text-white hover:bg-primary/5 hover:text-white"
+        >
+          Delete forever
+        </Button>
+      </ConfirmModal>
+    </div>
   );
 };
 
